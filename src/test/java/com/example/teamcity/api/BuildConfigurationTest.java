bdfb293ae789d925ed099632e245e32beebdbb3f
mikhailqa <mikhail.naskalov@gmail.com>
2025-02-21 01:31:30 +0100

    Add config, spec, test

---
+++ src/test/java/com/example/teamcity/api/BuildConfigurationTest.java
@@ -0,0 +1,6 @@
+package com.example.teamcity.api;
+
+import com.example.teamcity.api.api.BaseApiTest;
+
+public class BuildConfigurationTest extends BaseApiTest {
+}
---
+++ src/test/java/com/example/teamcity/api/api/BaseApiTest.java
@@ -0,0 +1,7 @@
+package com.example.teamcity.api.api;
+
+import com.example.teamcity.api.BaseTest;
+
+public class BaseApiTest extends BaseTest {
+}
+
